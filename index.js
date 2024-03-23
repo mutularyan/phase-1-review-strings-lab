972143927901deab64f45a44d9d42ae743bb1138
ryanmutula <ryan.mutula@student.moringaschool.com>
2024-03-23 12:38:17 +0300

    Strings lab 1 Javascript Strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ${G.slice(0, 1}!';
